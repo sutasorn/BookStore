0cd9cddd38a65066f01795fe7bdab3ebaf63c4ae
Sutasorn Yumak <sutasornza@gmail.com>
2022-06-09 21:26:07 +0700

    change netlify function path

--- controllers/error.js
+++ controllers/error.js
@@ -1,9 +1,4 @@
 exports.error404 = (req, res, next) => {
-    console.log("=================")
-    console.log("=================")
-    console.log("=================")
-    console.log("=================")
-    console.log("=================")
-    console.log("=================")
+
     res.status(404).render("404",{title:'Page Not Found',path:'/404',amountCart:0});
 }
