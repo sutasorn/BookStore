943c1be131488a7c68a494968cc2579fd293e5ab
Sutasorn Yumak <sutasornza@gmail.com>
2022-06-09 20:44:36 +0700

    build new

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,9 @@
 exports.error404 = (req, res, next) => {
+    console.log("=================")
+    console.log("=================")
+    console.log("=================")
+    console.log("=================")
+    console.log("=================")
+    console.log("=================")
     res.status(404).render("404",{title:'Page Not Found',path:'/404',amountCart:0});
 }
