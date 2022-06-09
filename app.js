a3fb59a8702547a41c08fe2b7662d86623edbe46
Sutasorn Yumak <sutasornza@gmail.com>
2022-06-09 14:52:22 +0700

    add netlify server router

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
-
+const router = express.Router();
 const errorControler = require('./controllers/error');
 const indexRouters = require('./routes/index');
 const cartRouters = require('./routes/cart');
@@ -23,5 +23,5 @@ app.use(cartRouters);
 
 app.use(errorControler.error404);
 app.listen(3000);
-
-module.exports.handler = serverless(app)
+app.use('/.netlify/functions/server',router);  // path must route to lambda (express/server.js)
+module.exports.handler = serverless(app);
